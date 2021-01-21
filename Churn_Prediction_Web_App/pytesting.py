42f8f960ce78f950fe26bc243dc1b505d1129565
Sukil Siva <61434077+sukilsiva@users.noreply.github.com>
2021-01-21 21:03:57 +0530

    Update pytesting.py

--- Churn_Prediction_Web_App/pytesting.py
+++ Churn_Prediction_Web_App/pytesting.py
@@ -22,7 +22,7 @@ class testing():
         #https://dev.to/fpim/object-oriented-design-architecture-with-panda-me4     ### Reference for data cleaning and making perfect data
         
         ### Have an Connection to Database
-        engine = sqlalchemy.create_engine('mysql+pymysql://root:idntknwpassword@localhost:3306/churnapp')
+        engine = sqlalchemy.create_engine('mysql+pymysql://root:idntknwpassword#404@localhost:3306/churnapp')
         avail_df = pd.read_sql_table(self.availdata, engine)
         app_df = pd.read_sql_table(self.webappdata, engine)
         
